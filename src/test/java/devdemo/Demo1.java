198ed4baa7c24828426e33e595dc10330f5685a0
Sonal <mittal.sonal04@gmail.com>
2020-06-04 21:00:49 -0400

    commit first git demo

--- src/test/java/devdemo/Demo1.java
+++ src/test/java/devdemo/Demo1.java
@@ -7,6 +7,9 @@ public class Demo1 {
 
 		
 		System.out.println("Hello world");
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 
 }
